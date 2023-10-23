5e968451d4031ea874875eac2f04b0b8c859e165
llja <lesterlim.ja@gmail.com>
2023-10-24 07:11:06 +0800

    feat:called the backend api

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
-import React from "react";
+import React, {useState,useEffect} from "react";
 import logo from "./logo.png";
 import "./App.css";
+import axios from "axios";
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-        </header>
-      </div>
-    );
-  }
+const App =()=> {
+  const [sightings,setSightings]=useState(null);
+
+  const getSightings = async() =>{
+    try{
+      const response =await axios.get(`http://localhost:3000/sightings`);
+      setSightings(response.data);
+    } catch (error){
+      console.log(error)
+    }
+  };
+  
+  useEffect(()=>{
+    getSightings();
+  },[])
+  return (
+    <div className="App">
+      <header className="App-header">
+        <ol>
+          {sightings?sightings.map((sighting,index)=>{
+            return <li>{`${sighting.YEAR} ${sighting.STATE}`}</li>
+          }):"Loading"}
+        </ol>
+      </header>
+    </div>
+  );
+  
 }
 
 export default App;
